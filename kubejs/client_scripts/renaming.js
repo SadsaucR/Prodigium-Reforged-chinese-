4ecb3f73d7235e7b957d16608b232a7c79a78270
SadsaucR <cks6023159@gmail.com>
2026-04-21 10:22:31 +0800

    調整任務文本

--- kubejs/client_scripts/renaming.js
+++ kubejs/client_scripts/renaming.js
@@ -4,54 +4,54 @@ ClientEvents.lang("zh_cn", (event) => {
   event.renameItem("sweet_charm_o_mine:iron_pocket", "鈷彈袋");
   event.renameItem("sweet_charm_o_mine:gold_pocket", "鈀彈袋");
   event.renameItem("companions:netherite_chains", "烈火石鏈");
-  event.renameItem("companions:netherite_dagger", "玄玉短劍");
+  event.renameItem("companions:netherite_dagger", "玄玉匕首");
 
   event.renameItem("gwrexpansions:netherite_sniper", "鳳凰狙擊槍");
   event.renameItem("gwrexpansions:netherite_shotgun", "鳳凰散彈槍");
-  event.renameItem("gwrexpansions:netherite_gatling", "鳳凰加特林");
+  event.renameItem("gwrexpansions:netherite_gatling", "鳳凰機關槍");
 
-  event.renameItem("wizards:arcane_robe_head", "天境巫師帽");
-  event.renameItem("wizards:arcane_robe_chest", "天境巫師袍（上）");
-  event.renameItem("wizards:arcane_robe_legs", "天境巫師袍（下）");
-  event.renameItem("wizards:arcane_robe_feet", "天境巫師靴");
+  event.renameItem("wizards:arcane_robe_head", "天堂巫師帽");
+  event.renameItem("wizards:arcane_robe_chest", "天堂巫師袍（上）");
+  event.renameItem("wizards:arcane_robe_legs", "天堂巫師袍（下）");
+  event.renameItem("wizards:arcane_robe_feet", "天堂巫師靴");
 
-  event.renameItem("wizards:fire_robe_head", "下界巫師帽");
-  event.renameItem("wizards:fire_robe_chest", "下界巫師袍（上）");
-  event.renameItem("wizards:fire_robe_legs", "下界巫師袍（下）");
-  event.renameItem("wizards:fire_robe_feet", "下界巫師靴");
+  event.renameItem("wizards:fire_robe_head", "地獄巫師帽");
+  event.renameItem("wizards:fire_robe_chest", "地獄巫師袍（上）");
+  event.renameItem("wizards:fire_robe_legs", "地獄巫師袍（下）");
+  event.renameItem("wizards:fire_robe_feet", "地獄巫師靴");
 
   //// Simple Weapons
   // Corinthium -> Adamantium
   event.renameItem("simple_weapons:corinthium_scimitar", "精鋼彎刀");
   event.renameItem("simple_weapons:corinthium_scythe", "精鋼大鐮");
-  event.renameItem("simple_weapons:corinthium_dagger", "精鋼短劍");
+  event.renameItem("simple_weapons:corinthium_dagger", "精鋼匕首");
   event.renameItem(
     "simple_weapons:corinthium_greatsword",
     "精鋼巨劍"
   );
   event.renameItem("simple_weapons:corinthium_spear", "精鋼長矛");
-  event.renameItem("simple_weapons:corinthium_fist", "精鋼虎爪刃");
-  event.renameItem("simple_weapons:corinthium_sickle", "精鋼鴉嘴錐");
+  event.renameItem("simple_weapons:corinthium_fist", "精鋼指虎");
+  event.renameItem("simple_weapons:corinthium_sickle", "精鋼鐮刀");
   event.renameItem("simple_weapons:corinthium_katana", "精鋼武士刀");
 
   // Netherite -> Onyx
   event.renameItem("simple_weapons:netherite_scimitar", "玄玉彎刀");
   event.renameItem("simple_weapons:netherite_scythe", "玄玉大鐮");
-  event.renameItem("simple_weapons:netherite_dagger", "玄玉短劍");
+  event.renameItem("simple_weapons:netherite_dagger", "玄玉匕首");
   event.renameItem("simple_weapons:netherite_greatsword", "玄玉巨劍");
   event.renameItem("simple_weapons:netherite_spear", "玄玉長矛");
-  event.renameItem("simple_weapons:netherite_fist", "玄玉虎爪刃");
-  event.renameItem("simple_weapons:netherite_sickle", "玄玉鴉嘴錐");
+  event.renameItem("simple_weapons:netherite_fist", "玄玉指虎");
+  event.renameItem("simple_weapons:netherite_sickle", "玄玉鐮刀");
   event.renameItem("simple_weapons:netherite_katana", "玄玉武士刀");
   event.renameItem("gwrexpansions:netherite_bullet", "玄玉子彈");
 
   // Fiery -> Ignitium
   event.renameItem("simple_weapons:fiery_scimitar", "烈火石彎刀");
   event.renameItem("simple_weapons:fiery_scythe", "烈火石大鐮");
-  event.renameItem("simple_weapons:fiery_dagger", "烈火石短劍");
+  event.renameItem("simple_weapons:fiery_dagger", "烈火石匕首");
   event.renameItem("simple_weapons:fiery_greatsword", "烈火石巨劍");
   event.renameItem("simple_weapons:fiery_spear", "烈火石長矛");
-  event.renameItem("simple_weapons:fiery_fist", "烈火石虎爪刃");
-  event.renameItem("simple_weapons:fiery_sickle", "烈火石鴉嘴錐");
+  event.renameItem("simple_weapons:fiery_fist", "烈火石指虎");
+  event.renameItem("simple_weapons:fiery_sickle", "烈火石鐮刀");
   event.renameItem("simple_weapons:fiery_katana", "烈火石武士刀");
 });
